c394a4555d89a1da04f85884efd3c2b5b418a600
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-24 16:24:23 +0000

    initial setup connecting to database, wrote a single query in server file to get properties from database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,14 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb',
+});
+
 
 /// Users
 
@@ -67,11 +76,15 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const queryString = `SELECT * FROM properties 
+limit $1`;
+let values = [limit];
+ return pool.query(queryString, values)
+ .then (res => res.rows)
+ .catch((error) => {
+    console.log(error.message)
+   });
+
 }
 exports.getAllProperties = getAllProperties;
 
